314f5d3199b2fa605b8fab2f8b4308892d4e6f12
janrendel <janrendel11@gmail.com>
2025-06-13 11:26:37 +0900

    css, html 기존 추가

---
+++ src/main/java/kr/ac/kopo/janrendel11/term_project_2401110271/TermProject2401110271Application.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.janrendel11.term_project_2401110271;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TermProject2401110271Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TermProject2401110271Application.class, args);
+    }
+
+}
---
+++ src/test/java/kr/ac/kopo/janrendel11/term_project_2401110271/TermProject2401110271ApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.janrendel11.term_project_2401110271;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TermProject2401110271ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
